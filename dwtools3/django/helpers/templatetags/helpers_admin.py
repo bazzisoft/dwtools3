5362bc7bd6ae78e02b0dc301c0a90e399f8bc281
Barak Shohat <barak@bazzisoft.com>
2015-06-14 12:58:42 +0300

    Added admin breadcrumbs helper tag

---
+++ dwtools3/django/helpers/templatetags/helpers_admin.py
@@ -0,0 +1,39 @@
+from django import template
+from django.apps import apps
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('helpers/admin/breadcrumbs.html')
+def render_breadcrumbs(title, app=None, model=None, instance=None, is_popup=False):
+    """
+    Renders the Django admin breadcrumb trail.
+ 
+    eg::
+        
+            {% render_breadcumbs 'My Page' app='auth' %}
+            {% render_breadcumbs 'My Page' model=User %}
+            {% render_breadcumbs 'My Page' instance=request.user %}
+    """
+    assert app or model or instance
+
+    if instance:
+        model = type(instance)
+
+    if model:
+        app = model._meta.app_label
+
+    if app:
+        app = apps.get_app_config(app)
+
+    return {
+        'title': title,
+        'app_label': app.label if app else None,
+        'app_name': app.verbose_name if app else None,
+        'model_name': model._meta.verbose_name_plural if model else None,
+        'model_urlname': 'admin:{}_{}_changelist'.format(app.label, model._meta.model_name) if model else None,
+        'instance': instance,
+        'instance_urlname': 'admin:{}_{}_change'.format(app.label, model._meta.model_name) if instance else None,
+        'is_popup': is_popup,
+    }
