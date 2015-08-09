219fb2f2b6a436746b801bd4d563ffdf6949355c
Barak Shohat <barak@bazzisoft.com>
2015-08-09 12:33:46 +0300

    Improved absolute_url to work with params

--- dwtools3/django/helpers/templatetags/helpers.py
+++ dwtools3/django/helpers/templatetags/helpers.py
@@ -11,6 +11,6 @@ def lookup(dct, key):
 
 
 @register.simple_tag(takes_context=True)
-def absolute_url(context, url):
-    url = resolve_url(url)
+def absolute_url(context, url, *args, **kwargs):
+    url = resolve_url(url, *args, **kwargs)
     return context['request'].build_absolute_uri(url)
