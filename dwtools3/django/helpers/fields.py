118a30cc7a3225bfb96d837143bcfc6e2c96bd91
Barak Shohat <barak@bazzisoft.com>
2016-01-07 20:38:57 +0200

    Improved StrippedCharField and StrippedTextField to also remove ascii control chars

--- dwtools3/django/helpers/fields.py
+++ dwtools3/django/helpers/fields.py
@@ -5,6 +5,13 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
+# ASCII 00-31 mapped to None for translation
+# Allow ASCII 10 = \n and 13 = \r
+CONTROL_CHAR_MAP = dict.fromkeys(range(32))
+del CONTROL_CHAR_MAP[10]
+del CONTROL_CHAR_MAP[13]
+
+
 class StrippedCharField(models.CharField):
     """
     Regular ``CharField`` but stripped of leading/trailing whitespace.
@@ -13,7 +20,7 @@ class StrippedCharField(models.CharField):
 
     def to_python(self, value):
         ret = super().to_python(value)
-        return ret.strip() if ret is not None else ret
+        return ret.strip().translate(CONTROL_CHAR_MAP) if ret is not None else ret
 
 
 class StrippedTextField(models.TextField):
@@ -24,8 +31,8 @@ class StrippedTextField(models.TextField):
 
     def to_python(self, value):
         ret = super().to_python(value)
-        return ret.strip() if ret is not None else ret
+        return ret.strip().translate(CONTROL_CHAR_MAP) if ret is not None else ret
 
     def get_prep_value(self, value):
         ret = super().get_prep_value(value)
-        return ret.strip() if ret is not None else ret
+        return ret.strip().translate(CONTROL_CHAR_MAP) if ret is not None else ret
