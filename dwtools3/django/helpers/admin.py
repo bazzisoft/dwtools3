dcd4c06e654e2d2eb276d530e048b4401bf3bbd7
Barak Shohat <barak@bazzisoft.com>
2018-07-23 13:08:21 +0300

    Updated deployment scripts & allow callable in @default_list_filter decorator.

--- dwtools3/django/helpers/admin.py
+++ dwtools3/django/helpers/admin.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from django.utils.http import urlencode
 
 
-def default_list_filter(**default_filters):
+def default_list_filter(callable=None, **default_filters):
     """
     Class decorator used to decorate a ModelAdmin class, and provide
     default list filter values when the page is initially opened.
@@ -14,13 +14,19 @@ def default_list_filter(**default_filters):
         class MyAdmin(admin.ModelAdmin)
             list_filter = ('is_active', 'my_enum',)
             ...
+
+    Can also pass a single `callable` function which takes `request` and returns a dict
+    of filter parameters.
     """
     def decorator(cls):
         old_changelist_view = getattr(cls, 'changelist_view')
         def new_changelist_view(self, request, *args, **kwargs):
             if urlsplit(request.META.get('HTTP_REFERER', '')).path != request.path and \
                     not request.META.get('QUERY_STRING'):
-                return redirect('{}?{}'.format(request.path, urlencode(default_filters)))
+                filters = callable(request) if callable else default_filters
+                print(filters)
+                if filters:
+                    return redirect('{}?{}'.format(request.path, urlencode(filters)))
             return old_changelist_view(self, request, *args, **kwargs)
         setattr(cls, 'changelist_view', new_changelist_view)
         return cls
