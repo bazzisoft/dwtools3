15beeb089167c00f3b7f2653f29b55d01e6c63ad
Barak Shohat <barak@bazzisoft.com>
2018-12-23 22:56:11 +0200

    Added profiler function to system tools

---
+++ dwtools3/system/profiler.py
@@ -0,0 +1,15 @@
+import cProfile
+import pstats
+import StringIO
+
+
+def profile_function(fn, sort_by_cumulative_time=True):
+    pr = cProfile.Profile()
+    pr.enable()
+    fn()
+    pr.disable()
+    s = StringIO.StringIO()
+    sortby = 'time' if not sort_by_cumulative_time else 'cumulative'
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    return s.getvalue()
