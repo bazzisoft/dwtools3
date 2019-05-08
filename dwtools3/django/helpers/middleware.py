c2dd8c4215e7fcf962d6e6a4b74b0ea5fcc5164e
Barak Shohat <barak@bazzisoft.com>
2019-05-08 14:35:41 +0300

    Improvements to PerformanceStatsMiddleware

--- dwtools3/django/helpers/middleware.py
+++ dwtools3/django/helpers/middleware.py
@@ -6,7 +6,7 @@ from django.http.response import HttpResponse
 from django.utils.html import escapejs
 
 
-def TranslateProxyRemoteAddrMiddleware(get_response):
+def TranslateProxyRemoteAddrMiddleware(get_response):  # pylint: disable=invalid-name
     """
     Proxy servers (eg. nginx -> gunicorn) tend to override
     the REMOTE_ADDR header.
@@ -64,17 +64,21 @@ class PerformanceStatsMiddleware:
         response = self.get_response(request)
 
         stats = getattr(request, self.STATS_KEY, None)
-        if stats is None or not stats['view_start']:
+        if (stats is None or not stats['view_start'] or not request.resolver_match or
+                request.resolver_match.view_name == 'django.views.static.serve'):
             return response
 
         stats['end'] = time()
         stats['total_time'] = stats['end'] - stats['start']
         stats['db_queries'] = len(connection.queries)
-        stats['db_time'] = functools.reduce(operator.add, (float(q['time']) for q in connection.queries), 0.0)
+        stats['db_time'] = functools.reduce(operator.add,
+                                            (float(q['time']) for q in connection.queries),
+                                            0.0)
         stats['python_time'] = stats['total_time'] - stats['db_time']
 
         stats['middleware_total_time'] = stats['view_start'] - stats['start']
-        stats['middleware_python_time'] = stats['middleware_total_time'] - stats['middleware_db_time']
+        stats['middleware_python_time'] = \
+            stats['middleware_total_time'] - stats['middleware_db_time']
 
         stats['view_total_time'] = stats['end'] - stats['view_start']
         stats['view_db_queries'] = stats['db_queries'] - stats['middleware_db_queries']
@@ -82,23 +86,33 @@ class PerformanceStatsMiddleware:
         stats['view_python_time'] = stats['view_total_time'] - stats['view_db_time']
 
         formatted = []
-        formatted.append('     STATS: total:{:4.0f}ms,  python:{:4.0f}ms,  db:{:4.0f}ms,  queries:{:3d}'
-                         .format(stats['total_time'] * 1000.0, stats['python_time'] * 1000.0,
-                                 stats['db_time'] * 1000.0, stats['db_queries']))
-        formatted.append('MIDDLEWARE: total:{:4.0f}ms,  python:{:4.0f}ms,  db:{:4.0f}ms,  queries:{:3d}'
-                         .format(stats['middleware_total_time'] * 1000.0, stats['middleware_python_time'] * 1000.0,
-                                 stats['middleware_db_time'] * 1000.0, stats['middleware_db_queries']))
-        formatted.append('      VIEW: total:{:4.0f}ms,  python:{:4.0f}ms,  db:{:4.0f}ms,  queries:{:3d}'
-                         .format(stats['view_total_time'] * 1000.0, stats['view_python_time'] * 1000.0,
-                                 stats['view_db_time'] * 1000.0, stats['view_db_queries']))
+        formatted.append('     STATS: total:{:4.0f}ms,  python:{:4.0f}ms,  '
+                         'db:{:4.0f}ms,  queries:{:3d}'
+                         .format(stats['total_time'] * 1000.0,
+                                 stats['python_time'] * 1000.0,
+                                 stats['db_time'] * 1000.0,
+                                 stats['db_queries']))
+        formatted.append('MIDDLEWARE: total:{:4.0f}ms,  python:{:4.0f}ms,  '
+                         'db:{:4.0f}ms,  queries:{:3d}'
+                         .format(stats['middleware_total_time'] * 1000.0,
+                                 stats['middleware_python_time'] * 1000.0,
+                                 stats['middleware_db_time'] * 1000.0,
+                                 stats['middleware_db_queries']))
+        formatted.append('      VIEW: total:{:4.0f}ms,  python:{:4.0f}ms,  '
+                         'db:{:4.0f}ms,  queries:{:3d}'
+                         .format(stats['view_total_time'] * 1000.0,
+                                 stats['view_python_time'] * 1000.0,
+                                 stats['view_db_time'] * 1000.0,
+                                 stats['view_db_queries']))
         formatted = '\n'.join(formatted)
 
         print('')
         print(formatted)
 
-        if (type(response) is HttpResponse and response['Content-Type'].startswith('text/html')
+        if (isinstance(response, HttpResponse) and response['Content-Type'].startswith('text/html')
                 and not response.get('Content-Disposition')):
-            newcontent = ('''<script>console.log(\'{}\');</script>\n</body>'''.format(escapejs(formatted)))
+            newcontent = ('''<script>console.log(\'{}\');</script>\n</body>'''
+                          .format(escapejs(formatted)))
             response.content = response.content.replace(b'</body>', newcontent.encode('utf-8'))
 
         return response
@@ -107,5 +121,6 @@ class PerformanceStatsMiddleware:
         stats = getattr(request, self.STATS_KEY)
         stats['view_start'] = time()
         stats['middleware_db_queries'] = len(connection.queries)
-        stats['middleware_db_time'] = functools.reduce(operator.add, (float(q['time']) for q in connection.queries), 0.0)
+        stats['middleware_db_time'] = functools.reduce(
+            operator.add, (float(q['time']) for q in connection.queries), 0.0)
         return None
