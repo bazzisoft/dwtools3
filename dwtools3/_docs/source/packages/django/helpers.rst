bacc59c8a04f046f410f937c5a178d873fe5426d
Barak Shohat <barak@bazzisoft.com>
2020-10-01 11:43:03 +0300

    Added form helper for maintaining current selections in an instance when no longer valid for selection on new items.

--- dwtools3/_docs/source/packages/django/helpers.rst
+++ dwtools3/_docs/source/packages/django/helpers.rst
@@ -7,46 +7,53 @@
 
 Application-Specific Settings
 -----------------------------
-   
+
 .. automodule:: dwtools3.django.helpers.settings
    :members:
 
-   
+
 Model Inheritance
 -----------------
 
 .. automodule:: dwtools3.django.helpers.inheritance
    :members:
 
-   
+
 Models
 ------
 
 .. automodule:: dwtools3.django.helpers.models
    :members:
-   
-   
+
+
+Forms
+-----
+
+.. automodule:: dwtools3.django.helpers.forms
+   :members:
+
+
 View Helper Functions
 ---------------------
 
 .. automodule:: dwtools3.django.helpers.view_helpers
    :members:
 
-   
+
 Raw SQL
 -------
 
 .. automodule:: dwtools3.django.helpers.sql
    :members:
 
-   
+
 Middleware
 ----------
 
 .. automodule:: dwtools3.django.helpers.middleware
    :members:
 
-   
+
 Logging
 -------
 
---
+++ dwtools3/django/helpers/forms.py
@@ -0,0 +1,40 @@
+"""
+Useful form mixins.
+"""
+from django import forms
+
+
+class KeepCurrentInstanceChoicesModelFormMixin:
+    """
+    ``ModelForm`` mixin that finds all ``ModelChoiceFields`` and ``ModelMultipleChoiceFields``
+    in the form, and ensures all values currently assigned to the instance
+    are still selectable on save (even if they are disallowed due to the
+    specified querysets.)
+
+    For example, a user that has been deactivated can still be saved on the
+    form for existing instances, but not for new ones.
+
+    **IMPORTANT**: This mixin must be inherited *before* ``ModelForm``.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        assert self.instance, 'This mixin can only be used with ModelForms.'
+        if not self.instance.id:
+            return
+
+        for fieldname, field in self.fields.items():
+            if isinstance(field, (forms.ModelChoiceField, forms.ModelMultipleChoiceField)):
+                curval = getattr(self.instance, fieldname, None)
+                if curval:
+                    if hasattr(curval, 'pk'):
+                        field.queryset = (
+                            field.queryset | curval.__class__.objects.filter(pk=curval.pk)
+                        )
+                    elif hasattr(curval, 'target_field'):
+                        field.queryset = (
+                            field.queryset |
+                            curval.target_field.related_model.objects.filter(pk__in=curval.all())
+                        )
+                    else:
+                        # TODO: Other cases we need to handle?
+                        pass
