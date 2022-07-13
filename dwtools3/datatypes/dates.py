eb02ea99a8c10d26413c6568fd880a643eae5f50
Barak Shohat <barak@bazzisoft.com>
2022-07-13 17:31:57 +0300

    improved unix timestamp conversion

--- dwtools3/datatypes/dates.py
+++ dwtools3/datatypes/dates.py
@@ -1,7 +1,7 @@
 """
 Utility functions for working with dates and timezones.
 """
-from datetime import datetime, timedelta, tzinfo
+from datetime import datetime, timedelta, tzinfo, time
 
 
 class UTCTZInfo(tzinfo):
@@ -29,9 +29,18 @@ UTC = UTCTZInfo()
 EPOCH = datetime(1970, 1, 1, tzinfo=UTC)
 
 
+def date_to_unix_timestamp(dt):
+    dt = datetime.combine(dt, time.min.replace(tzinfo=UTC))
+    return datetime_to_unix_timestamp(dt)
+
+
 def datetime_to_unix_timestamp(dt):
-    return (dt - EPOCH).total_seconds()
+    return int((dt - EPOCH).total_seconds())
 
 
 def unix_timestamp_to_datetime(timestamp):
     return datetime.fromtimestamp(timestamp, UTC)
+
+
+def unix_timestamp_to_date(timestamp):
+    return unix_timestamp_to_datetime(timestamp).date()
