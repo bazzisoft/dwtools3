5a154290776d44af7d695714f3ca766a7ab21b37
Barak Shohat <barak@bazzisoft.com>
2025-10-20 17:13:05 +0300

    Release for v3.1.1

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
+
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 
@@ -37,7 +38,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="3.1.0",
+    version="3.1.1",
     description=short_description,
     long_description=long_description,
     # The project's main homepage.
