46f6e381c092db97efcbb011a82a000be969f102
Barak Shohat <barak@bazzisoft.com>
2025-10-22 16:22:28 +0300

    error update

--- dwtools3/django/helpers/sql.py
+++ dwtools3/django/helpers/sql.py
@@ -33,12 +33,12 @@ class RawSQLBuilder:
     def _check_params(self, params):
         if isinstance(params, dict):
             assert all(not isinstance(p, tuple) for p in params.values()), (
-                "tuple sql parameters not supported, use lists which are SQL arrays "
+                "tuple sql parameters not supported, use lists which are sql arrays "
                 "(possibly with `=ANY(%s)`)"
             )
         elif isinstance(params, (list, tuple)):
             assert all(not isinstance(p, tuple) for p in params), (
-                "tuple sql parameters not supported, use lists which are SQL arrays "
+                "tuple sql parameters not supported, use lists which are sql arrays "
                 "(possibly with `=ANY(%s)`)"
             )
 
